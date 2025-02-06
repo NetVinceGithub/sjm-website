fafb6e9fbf9b7af648af34a3522cef064dae25ad
Vinceeeee <vjmalicsi08@gmail.com>
2025-02-06 09:49:40 +0800

    push before advancing

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,29 +1,15 @@
 import express from "express";
-import path from "path";
-import authMiddleware from "../middleware/authMiddleware.js";
 import {
-  addEmployee,
-  uploadFields,
-  getEmployee,
+  
   getEmployees,
-  updateEmployee,
+  importEmployeesFromGoogleSheet,
 } from "../controllers/employeeController.js";
 
 const router = express.Router();
 
-// Serve static files from the 'uploads' folder
-router.use("/uploads", express.static(path.resolve("uploads")));
-
-// Fetch all employees
-router.get("/", authMiddleware, getEmployees);
-
-// Fetch a single employee by ID
-router.get("/:id", authMiddleware, getEmployee);
-
-// Add a new employee with file upload
-router.post("/add", authMiddleware, uploadFields, addEmployee);
-
-// Update an employee with file upload
-router.put("/:id", authMiddleware, uploadFields, updateEmployee);
+router.get("/", getEmployees);
+//router.get("/:id", getEmployee);
+//router.post("/add", addEmployee);
+router.get("/import", importEmployeesFromGoogleSheet); // Manual Sync API
 
 export default router;
