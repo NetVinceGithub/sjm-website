28aa131a335fac09d4db5995572c3f119b6ec553
Vinceeeee <vjmalicsi08@gmail.com>
2025-03-09 17:30:44 +0800

    last edit before ang meet mamayang gabi

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { saveAttendance, saveAttendanceSummary, getAttendance, getAttendanceSummary, getAttendanceHistory } from "../controllers/attendanceController.js";
+import { saveAttendance, saveAttendanceSummary, getAttendance, getAttendanceSummary, getAttendanceHistory, deleteAllAttendance } from "../controllers/attendanceController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post("/add-attendance-summary", saveAttendanceSummary);
 router.get("/get-attendance", getAttendance);
 router.get("/get-history", getAttendanceHistory);
 router.get("/get-summary", getAttendanceSummary);
+router.delete("/delete-all-attendance", deleteAllAttendance);
+
 
 export default router;
