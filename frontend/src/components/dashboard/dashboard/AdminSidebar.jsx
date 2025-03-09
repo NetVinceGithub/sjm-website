d85b508fd2373b0b824a4f9e5f124e586dee3448
Vinceeeee <vjmalicsi08@gmail.com>
2025-03-09 14:47:34 +0800

    applied fixes

--- frontend/src/components/dashboard/dashboard/AdminSidebar.jsx
+++ frontend/src/components/dashboard/dashboard/AdminSidebar.jsx
@@ -198,6 +198,18 @@ const AdminSidebar = () => {
                       <FaCalendarPlus />
                       <span>Add Attendance</span>
                     </NavLink>
+                    <NavLink
+                      to="/admin-dashboard/attendance-computation"
+                      className={({ isActive }) =>
+                        `flex -mt-1 items-center space-x-4 text-white no-underline py-2.5 px-4 rounded-md transition ${
+                          isActive ? "bg-[#5f2e3d]" : "hover:bg-[#924F64]"
+                        }`
+                      }
+                      end
+                    >
+                      <FaCalendarPlus />
+                      <span>Attendance for Computation</span>
+                    </NavLink>
 
                     <NavLink
                       to="/admin-dashboard/attendance/history"
--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { saveAttendance, saveAttendanceSummary, getAttendance } from "../controllers/attendanceController.js";
+import { saveAttendance, saveAttendanceSummary, getAttendance, getAttendanceSummary, getAttendanceHistory } from "../controllers/attendanceController.js";
 
 const router = express.Router();
 
 router.post("/add-attendance", saveAttendance);
 router.post("/add-attendance-summary", saveAttendanceSummary);
 router.get("/get-attendance", getAttendance);
+router.get("/get-history", getAttendanceHistory);
+router.get("/get-summary", getAttendanceSummary);
 
 export default router;
