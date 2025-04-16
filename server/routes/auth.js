0d05507ffad373d0aa10487de3ed501d74e0d18f
Vinceeeee <vjmalicsi08@gmail.com>
2025-04-16 22:40:20 +0800

    nag lagay ako ng remember me button, function para sa forget password, block at unblock button sa mga user at edit. Naayos ko na din yung kapag naga resresh ang punta ay sa login, kada refresh hindi siya naga logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,16 @@
 import express from 'express'
-import { login, verify } from '../controllers/authController.js';
+import { login, verify, forgotPassword, resetPassword, verifyCode } from '../controllers/authController.js';
 
-// Ensure file paths are accurate and no typos exist.
 
 import authMiddleware from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.post('/login', login )
-router.get('/verify', authMiddleware, verify)
+router.post('/login', login );
+router.get('/verify', authMiddleware, verify);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
+router.post('/verify-code', verifyCode);
+
 
 export default router;
