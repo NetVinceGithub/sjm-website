b46c415e4bf47c90f6356310e1ad2d73c4e4b529
Vinceeeee <vjmalicsi08@gmail.com>
2025-04-16 14:43:11 +0800

    gawa lang muna ako backup para dun sa idea ko sa jobs

--- server/index.js
+++ server/index.js
@@ -99,12 +99,6 @@ app.use((req, res, next) => {
 // Serve frontend's public folder as static
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-// Example route to test access
-
-
-// Start server
-app.listen(5000, () => console.log("Server running on port 5000"));
-
 
 // Your existing routes go here
 app.use("/api/auth", authRouter);
@@ -117,6 +111,10 @@ app.use("/api/attendance", attendanceRouter);
 app.use("/api/connect", connectRouter); // Ensure this line is included
 app.use("/api/holidays", holidaysRouter)
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+})
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
