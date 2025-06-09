57c001df1855ef482434ef73ea3d7e670fe68048
Vinceeeee <vjmalicsi08@gmail.com>
2025-06-09 13:04:58 +0800

    payslip

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/public", express.static(path.join(__dirname, "../frontend/public")));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
