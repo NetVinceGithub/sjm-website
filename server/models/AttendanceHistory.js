54c15ca1472d017a131dacb9c74cb82c7e3cd33e
Vinceeeee <vjmalicsi08@gmail.com>
2025-06-10 16:55:02 +0800

    Pushing the server file

--- server/models/AttendanceHistory.js
+++ server/models/AttendanceHistory.js
@@ -2,82 +2,91 @@ import { DataTypes } from "sequelize";
 import sequelize from "../db/db.js";
 import moment from "moment";
 
-const AttendanceHistory = sequelize.define("AttendanceHistory", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  ecode: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  date: {
-    type: DataTypes.DATEONLY,
-    allowNull: false,
-  },
-  dutyStart: {
-    type: DataTypes.TIME,
-    allowNull: true,
-    field: 'duty_start'
-  },
-  dutyEnd: {
-    type: DataTypes.TIME,
-    allowNull: true,
-    field: 'duty_end'
-  },
-  punchIn: {
-    type: DataTypes.TIME,
-    allowNull: true,
-    field: 'punch_in'
-  },
-  punchOut: {
-    type: DataTypes.TIME,
-    allowNull: true,
-    field: 'punch_out'
-  },
-  workTime: {
-    type: DataTypes.TIME,
-    allowNull: true,
-    field: 'work_time'
-  },
-  lateTime: {
-    type: DataTypes.TIME,
-    allowNull: true,
-    field: 'late_time'
-  },
-  overtime: {
-    type: DataTypes.TIME,
-    allowNull: true,
-    field: 'overtime'
-  },
-  absentTime: {
-    type: DataTypes.TIME,
-    allowNull: true,
-    field: 'absent_time'
-  },
-  isAbsent: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-    field: 'is_absent'
-  },
-  status: {
-    type: DataTypes.ENUM('present', 'absent', 'late', 'holiday'),
-    defaultValue: 'present'
-  },
-}, {
-  tableName: 'attendancehistory',
-  timestamps: true,
-  // Remove the unique index to allow duplicates
-  indexes: [
-    // You can add non-unique indexes for performance if needed
-    {
-      fields: ['ecode']
+const AttendanceHistory = sequelize.define(
+  "AttendanceHistory",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      fields: ['date']
-    }
-  ]
-});
+    ecode: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    dutyStart: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      field: "duty_start",
+    },
+    dutyEnd: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      field: "duty_end",
+    },
+    punchIn: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      field: "punch_in",
+    },
+    punchOut: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      field: "punch_out",
+    },
+    workTime: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      field: "work_time",
+    },
+    lateTime: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      field: "late_time",
+    },
+    overtime: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      field: "overtime",
+    },
+    absentTime: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      field: "absent_time",
+    },
+    isAbsent: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      field: "is_absent",
+    },
+    status: {
+      type: DataTypes.ENUM("present", "absent", "late", "holiday"),
+      defaultValue: "present",
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "attendancehistory",
+    timestamps: true,
+    // Remove the unique index to allow duplicates
+   indexes: [
+    { fields: ['ecode'] },
+    { fields: ['date'] }
+  ],
+  }
+);
 
 export default AttendanceHistory;
