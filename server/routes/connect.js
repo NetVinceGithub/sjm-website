7db7076a16a081169f7323e9a8f95012189a626e
vince <vjmalicsi08@gmail.com>
2025-07-19 16:06:55 +0800

    laravel server

--- server/routes/connect.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addMessages, getMessages } from "../controllers/connectController.js";
-const router = express.Router();
-
-// POST request to save form data
-router.post('/', addMessages); // The route should be '/' since it is prefixed by '/api/connect' in index.js
-router.get('/messages', getMessages);
-export default router;
--- server/routes/invoice.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getInvoices } from '../controllers/invoiceController.js';
-
-const router = express.Router();
-
-router.get("/", getInvoices); 
-
-export default router;
